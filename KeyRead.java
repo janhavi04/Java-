d7b03560250f84ae9c9b23233be0d82ce2ad7e72
janhavi04 <jumblejanhavi@gmail.com>
2023-09-21 17:39:38 +0530

    edit

---
+++ KeyRead.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.util.*;
+
+class KeyRead{
+    public static void main(String args[]){
+        //Scanner s = new Scanner(System.in);
+       // int a,b,c;
+       // System.out.println("Enter 2 nos:");
+      //  a=s.nextInt();
+      //  b=s.nextInt();
+      //  c = a+b;
+       // System.out.print("Sum is " + c);
+        //String name;
+       // System.out.println("May I know your name?");
+       // name = s.nextLine();
+        //System.out.println("Welcome Mr/MIss " + name);
+        //System.out.println("Integer min value " + Integer.MIN_VALUE);
+        //System.out.println("integer max value " + Integer.MAX_VALUE);
+        //System.out.println("Integer Bytes  " + Integer.BYTES);
+        
+        long l = 999_999_999_999L;
+        float f = 12.56f;
+
+        System.out.println(l);
+        System.out.println(f);
+    }
+
+}
