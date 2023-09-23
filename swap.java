b5271859ebf4fe22249b5e3b1a10a8403f41b015
janhavi04 <jumblejanhavi@gmail.com>
2023-09-23 17:48:28 +0530

    edit

---
+++ swap.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class swap{
+
+    public static void main(String args[]){
+        int a,b;
+        System.out.println("Enter 2 numbers to swap:");
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println("After swapping a is " +a+" and b is "+b);
+    }
+}
