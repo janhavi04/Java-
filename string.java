f66323090b979834059e520f30871506271619a0
janhavi04 <jumblejanhavi@gmail.com>
2023-09-27 21:19:11 +0530

    edit

---
+++ string.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+public class string{
+
+    public static void main(String[] args){
+        // String str1 = "Java Program";
+        // String str2= new String ("Java");
+        // System.out.println(str1 + " " + str2);
+
+        // char c[] = {'A', 'B','C','D'};
+
+        // String str3 = new String(c,1,3);
+        // System.out.println(str3);
+               
+        // byte b[] = {65, 66,67,68};
+
+        // String str4 = new String(b,1,2);
+        // System.out.println(str4);
+
+        String str1 = "java";
+        String str2 = new String("java") ;  
+     System.out.println(str1 == str2);
+
+  }
+}
