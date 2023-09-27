3baf2db413af38b6f057f1b40406946b6152c740
janhavi04 <jumblejanhavi@gmail.com>
2023-09-27 21:21:08 +0530

    practice

---
+++ maskingAndMerging.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.Scanner;
+
+class swap{
+
+    public static void main(String args[]){
+        int a=9,b=12;
+        
+        int c;
+        c = a<<4;
+        c = c|b;
+
+        System.out.println((c&0b11110000)>>4);
+         System.out.println(c&0b11110000);
+    
+    }
+    
+    
+    
+    }
