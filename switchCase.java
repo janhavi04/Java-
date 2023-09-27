60a620d836759a5d30e10ebaed75f20d18332aa8
janhavi04 <jumblejanhavi@gmail.com>
2023-09-27 21:18:22 +0530

    edit

---
+++ switchCase.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/**
+ * switch
+ */
+//Create a C++ program that uses a switch-case statement to calculate the grade of a student based on their percentage score.
+// // The program should take the percentage as input and then display the corresponding grade (A, B, C, D, or F).
+// public class switchCase {
+
+//     public static void percent(int marks){
+//        int grade;
+//         if(marks>=90)
+//            grade = 'A';
+//         else if(marks>=80 && marks <90)
+//              grade = 'B';
+//         else if(marks>=70 && marks <80)
+//              grade = 'C';
+//         else if(marks>=60 && marks <70)
+//              grade = 'D';
+//         else
+//             grade = 'F';
+
+//             switch(grade){
+//                 case 'A':
+//                 System.out.println("Excellent");
+//                 break;
+//                 case 'B':
+//                 System.out.println("Good");
+//                 break;
+//                 case 'C':
+//                 System.out.println("Satisfactory");
+//                 break;
+//                 case 'D':
+//                 System.out.println("Average");
+//                 break;
+//                 case 'F':
+//                 System.out.println("Fail");break;
+//                 default:
+//                 System.out.println("Invalid percentage");
+
+//             }
+//     }
+//     public static void main(String[] args){
+
+//         int marks;
+//         Scanner s = new Scanner(System.in);
+//         System.out.println("Enter marks:");
+//         marks = s.nextInt();
+//         percent(marks);
+
+//     }
+// }
+
+//Write a java program that uses a switch-case statement to take an integer input (1 to 12) representing a month and
+// then displays the number of days in that month. Consider leap years.
+/**
+ * switchCase
+ */
+
