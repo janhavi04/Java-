1af2f7c1ab08fdbe130e2c88086f66285c11736a
janhavi04 <jumblejanhavi@gmail.com>
2023-09-28 16:50:50 +0530

    Completed

--- arrayPractice.java
+++ arrayPractice.java
@@ -52,38 +52,42 @@ public class arrayPractice {
 
 
 
-        // int A[] = {5,9,6,10,12,7,3,5,4,2};
-        // int temp = A[A.length-1];
+         //int A[] = {5,9,6,10,12,7,3,5,4,2};
+        // int temp = A[0];
     
 
         // for(int x:A)
         //         System.out.print(x+" ");
         //     System.out.println(" ");
-        //     //Rotating an array
-        //     //FOR LEFT SHIFT
-        // // for(int i =1; i<A.length;i++){ 
-        // //     A[i-1]= A[i];
+        // //   Rotating an array
+        // //   FOR LEFT SHIFT
+        // for(int i =1; i<A.length;i++){ 
+        //     A[i-1]= A[i];
 
-        // // }
+        // }
 
-        // // A[A.length-1]=temp;
-
-        // //  for(int x:A)
-        // //         System.out.print(x+" ");
-        // //     System.out.println(" ");
-
-        // //FOR RIGHT SHIFT
-
-        // for(int i =A.length-1; i>0;i--){ 
-        //     A[i] = A[i-1];
-
-        //  }
-
-        //  A[0] = temp;
+        // A[A.length-1]=temp;
 
         //  for(int x:A)
-        //        System.out.print(x+" ");
-        // System.out.println(" ");
+        //         System.out.print(x+" ");
+        //     System.out.println(" ");
+
+        // //FOR RIGHT SHIFT
+        int A[] = {5,9,6,10,12,7,3,5,4,2};
+         int temp = A[A.length-1];
+            for(int x:A)
+                 System.out.print(x+" ");
+             System.out.println(" ");
+        for(int i =A.length-1; i>0;i--){ 
+            A[i] = A[i-1];
+
+         }
+
+         A[0] = temp;
+
+         for(int x:A)
+               System.out.print(x+" ");
+        System.out.println(" ");
             
         //Inserting an element
 
@@ -172,22 +176,22 @@ public class arrayPractice {
             //  }
         //Increasing size of an Array
 
-            int A[] = {8,6,10,9,2};
-            int B[] = new int[2*A.length];
-                System.out.println(A.length);
+            // int A[] = {8,6,10,9,2};
+            // int B[] = new int[2*A.length];
+            //     System.out.println(A.length);
 
-            for(int i = 0 ; i<A.length;i++){
-                B[i] = A[i];
-            }
-            A = B;
-            B = null;
+            // for(int i = 0 ; i<A.length;i++){
+            //     B[i] = A[i];
+            // }
+            // A = B;
+            // B = null;
 
-            // for(int x :B){
-            //     System.out.print(x + " ");
-            //   }
-            //   System.out.println(" ");
+            // // for(int x :B){
+            // //     System.out.print(x + " ");
+            // //   }
+            // //   System.out.println(" ");
 
-              System.out.println(A.length);
+            //   System.out.println(A.length);
 
     }
 }
