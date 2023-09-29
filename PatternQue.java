4afaa2c12fa31d5a4a96a64e75977a047390b475
janhavi04 <jumblejanhavi@gmail.com>
2023-09-29 15:19:16 +0530

    edit

---
+++ PatternQue.java
@@ -0,0 +1,205 @@
+/**
+ * PatternQue
+ */
+public class PatternQue {
+
+    public static void main(String[] args){
+
+        // System.out.println("Printing column value");
+        // for(int i = 1;i<=5;i++){
+        //     for(int j = 1; j<=5; j++){
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        // System.out.println("");
+        
+        
+        // System.out.println("Printing row value");
+        // for(int i = 1;i<=5;i++){
+        //     for(int j = 1; j<=5; j++){
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        //     System.out.println("");
+
+
+        // System.out.println("Printing i+j");
+        //  for(int i = 1;i<=5;i++){
+        //     for(int j = 1; j<=5; j++){
+        //         System.out.print(i+j + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        // System.out.println("");
+
+        
+
+
+        // System.out.println("Printing numbers from 1 to 25");
+        //  int count = 0;
+        // for(int i = 1;i<=5;i++){
+        //     for(int j = 1; j<=5; j++){
+        //         System.out.format("%02d " , ++count);
+        //     }
+        //     System.out.println("");
+        // }
+          //   System.out.println("");
+
+
+           
+           // System.out.println("Next pattern:");
+            //1
+            //1 2 
+            //1 2 3
+            //1 2 3 4
+            //1 2 3 4 5
+          
+    //     for(int i = 1; i<=5 ; i++){
+    //         for (int j = 1; j<=i;j++){
+    //             System.out.print(j + " ");
+
+    //         }
+    //         System.out.println("");
+    //     }
+    //    System.out.println("");
+
+            //1
+            //2 3 
+            //4 5 6 
+            //7 8 9 10
+            //11 12 13 14 15
+          
+        //     int count = 0;
+        //      for(int i = 1; i<=5 ; i++){
+                
+        //     for (int j = 1; j<=i ;j++){
+        //         count++;
+        //         System.out.format("%02d " ,count);
+
+        //     }
+        //     System.out.println("");
+        // }
+
+            //  System.out.println("Printing *");
+            //  for(int i = 1; i<=5 ; i++){
+                
+            // for (int j = 1; j<=i ;j++){
+                
+            //     System.out.print("*" + " ");
+            // }
+            //     System.out.println("");
+            // }
+
+
+                //1 2 3 4 5
+                //1 2 3 4 
+                //1 2 3 
+                //1 2 
+                //1
+
+
+
+            //     int count = 0;
+            //  for(int i = 1; i<=5 ; i++){
+                
+            // for (int j = 1; j<=5-i+1 ;j++){
+            //     count ++;
+            //     System.out.format("%02d " , count);
+            // }
+            //     System.out.println("");
+            // }
+
+
+
+            // for(int i = 1; i<=5 ; i++)
+            // {
+            //     for (int j = 1; j<=5 ;j++){
+               
+            //     if(i<=j)
+            //         System.out.print(("* "));
+            //     else    
+            //         System.out.print("  ");
+            // }
+            //     System.out.println("");
+            // }
+
+
+
+            // for(int i = 1; i<=5 ; i++)
+            // {
+            //     for (int j = 1; j<=5 ;j++){
+               
+            //     if(i+j >5)
+            //         System.out.print(("* "));
+            //     else    
+            //         System.out.print("  ");
+            // }
+            //     System.out.println("");
+            // }
+
+
+
+            //  for(int i = 1; i<=4 ; i++)
+            // {
+            //     System.out.print(" ");
+
+            //     for (int j = 1; j<=4 ;j++){
+               
+            //     if(i<=j)
+            //         System.out.print(("*"+ " "));
+            //     else    
+            //         System.out.print("  ");
+
+            //     }
+
+            //     for(int k = 1; k<=3; k++){
+            //         if(i+k<5){
+            //             System.out.print("* ");
+            //         }
+            //         else    
+            //             System.out.print(" ");
+            //     }
+            //     System.out.println("");
+            // }            
+            
+            
+
+            //Hollow Rectangle
+
+            
+        // for(int i =1;i<=3;i++){
+        //     for(int j = 1 ; j<=4 ; j++){
+        //         if(i == 1 || i == 3 || j == 1 ||  j == 4){
+        //             System.out.print("*");
+        //         }
+        //         else
+        //             System.out.print(" ");
+        //     }
+        //     System.out.println("");
+       // }
+
+        //Number Triangular
+
+        // for(int i =1 ; i<=4 ; i++){
+        //     for(int k = 1 ; k<=(4-i); k++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j =1; j<=i ; j++){
+        //         System.out.print(i + " ");
+
+
+        //     }
+        //     System.out.println("");
+        // }
+       
+        //Number Increasing Pyramid 
+
+}
+
+
+}
+
+
+    
