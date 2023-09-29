462cfe940e9ff9e534c6badf3ba850effd157a73
janhavi04 <jumblejanhavi@gmail.com>
2023-09-29 15:45:42 +0530

    Editing

--- PatternQue.java
+++ PatternQue.java
@@ -194,7 +194,47 @@ public class PatternQue {
         //     System.out.println("");
         // }
        
-        //Number Increasing Pyramid 
+        //NUmber Increasing Pyramid
+        // for(int i =1 ; i<=4 ; i++){
+          
+        //     for(int j =1; j<=i ; j++){
+        //         System.out.print(j + " ");
+
+
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        //Number Increasing Reverse Pyramid 
+
+        // int count = 0;
+        // for(int i =1 ; i<=4 ; i++){
+            
+        //     for(int j =1; j<=4-count ; j++){
+                
+        //         System.out.print(j+" ");
+
+
+        //     }
+        //     count++;
+        //     System.out.println("");
+        // }
+
+        //Number Changing Pyramid
+            int count =0;
+          for(int i =1 ; i<=4 ; i++){
+          
+            for(int j =1; j<=i ; j++){
+                count++;
+                System.out.print(count + " ");
+
+
+            }
+            System.out.println("");
+        }
+
+       
 
 }
 
