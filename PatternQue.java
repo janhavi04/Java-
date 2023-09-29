25d99df4bb06b1ad25484a5ec7410fb69abda0c4
janhavi04 <jumblejanhavi@gmail.com>
2023-09-29 16:25:41 +0530

    Edit

--- PatternQue.java
+++ PatternQue.java
@@ -222,17 +222,88 @@ public class PatternQue {
         // }
 
         //Number Changing Pyramid
-            int count =0;
-          for(int i =1 ; i<=4 ; i++){
+        //     int count =0;
+        //   for(int i =1 ; i<=4 ; i++){
           
-            for(int j =1; j<=i ; j++){
-                count++;
-                System.out.print(count + " ");
+        //     for(int j =1; j<=i ; j++){
+        //         count++;
+        //         System.out.print(count + " ");
 
 
-            }
-            System.out.println("");
+        //     }
+        //     System.out.println("");
+        // }
+
+
+        //Zero-One Triangle
+        
+        //  for(int i =1 ; i<=4 ; i++){
+          
+        //     for(int j =1; j<=i ; j++){
+        //         if((i+j )%  2 == 0)
+        //         System.out.print(1+ " ");
+        //         else 
+        //         System.out.print(0+" ");
+        //     }
+
+        //     System.out.println("");
+        
+        // }
+
+        //Palindrome Triangular
+    //    int n = 6;
+    //         for(int i =1; i<=n; i++){
+
+    //             for(int j = 1;j<=2*(n-i); j++){
+    //                 System.out.print(" ");
+    //             }
+    //             for(int j = i; j>=i; j--){
+    //                 System.out.print(j+" ");
+    //             }
+    //             for(int j = 2; j<=i; j++){
+    //                 System.out.print(j+" ");
+    //             }
+    //             System.out.println("");
+    //         }
+
+    
+    //Rhombus Pattern
+    int n = 6;
+    for(int i =n ; i>=1; i--){
+        for(int k = 1; k<=(n-i); k++){
+            System.out.print(" ");
         }
+        for(int j =1; j<=n; j++){
+            System.out.print("*"+" ");
+        }
+        System.out.println("");
+    }
+
+    //Diamond Pattern
+        // int  n =6;
+        // for(int i =1; i<=n;i++){
+        //     for(int k =1; k<=(n-i);k++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*" + " ");
+        //     }
+        //     System.out.println("");
+        // }
+
+        // for(int i =n-1; i>=1;i--){
+        //     for(int k =1; k<=(n-i);k++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*" + " ");
+        //     }
+        //     System.out.println("");
+        // }
+        
+        
+        
+       
 
        
 
