7e1aa50a5b5be60b9f780cc6bbe5f90703067369
janhavi04 <jumblejanhavi@gmail.com>
2023-10-03 19:47:54 +0530

    Practice

---
+++ InterfaceStudentChallenge.java
@@ -0,0 +1,44 @@
+/**
+ * InterfaceStudentChallenge
+ */
+
+interface Member {
+    void callback();
+ }
+ class Store{
+    Member mem[] = new Member[100];
+    int count =0;
+
+    void register(Member m){
+        mem[count++]= m;
+    }
+    void inviteSale(){
+        for(int i =0; i<count; i++){
+            mem[i].callback();
+        }
+    }
+ }
+
+ 
+
+ class Customer implements Member{
+    String name;
+
+    public Customer(String n){
+        name = n;
+   }
+    public void callback(){
+        System.out.println("OKAY, I will visit.-" + name);
+    }
+ }
+public class InterfaceStudentChallenge {
+
+    public static void main(String[] args){
+        Store s = new Store();
+        Customer c1 = new Customer("John");
+        Customer c2 = new Customer("SMith");
+        s.register(c1);
+        s.register(c2);
+        s.inviteSale();
+    }
+}
