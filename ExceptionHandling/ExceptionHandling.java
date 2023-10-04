81b7528e6e034d7538b038a64f1227c640c8c0f1
janhavi04 <jumblejanhavi@gmail.com>
2023-10-04 17:49:34 +0530

    Class file

---
+++ ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+
+
+public class ExceptionHandling {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ accessModifier.java
@@ -0,0 +1,9 @@
+/**
+ * accessModifier
+ */
+public class accessModifier {
+
+    public static void main(String [] args){
+        
+    }
+}
